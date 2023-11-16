7b9ab87f7e93c4a35a3fa781f1d5672ecb78dc7e
KornTK <pokde111@gmail.com>
2023-11-16 14:18:52 +0700

    save to SD card

--- Pro-IOT/Pro-IOT.ino
+++ Pro-IOT/Pro-IOT.ino
@@ -1,16 +1,24 @@
 #define BUZZER_PIN 16
 
+#define LINE_TOKEN "T49b5eqRrJhE1HMTguRVAmvI4tblu264TiHIcOtv0n9"
+
 #define BLYNK_TEMPLATE_ID "TMPL6J2b7dpxe"
 #define BLYNK_TEMPLATE_NAME "Quickstart Template"
 #define BLYNK_AUTH_TOKEN "r6fAQINtrC8GFhyFBgK7W6fbKfxcXOpm"
 #define BLYNK_PRINT Serial
+
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 #include <ESP8266HTTPClient.h>
+#include <TridentTD_LineNotify.h>
+#include <SPI.h>
+#include <SD.h>
 
 char ssid[] = "KornTK-2.4G";
 char pass[] = "pokde111*";
 String APIserver = "https://iot.korntk.com/iot-pro1/iot-api1.php?";
+File myFile; // สร้างออฟเจก File สำหรับจัดการข้อมูล
+const int chipSelect = 4;
 
 int analogPin = A0;
 int sensor_Flame = 5; // Assuming the flame sensor is connected to pin D5
@@ -22,6 +30,27 @@ void setup() {
   Serial.begin(9600);
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
   pinMode(BUZZER_PIN, OUTPUT);
+  
+  Serial.println(LINE.getVersion());
+  LINE.setToken(LINE_TOKEN);   // กำหนด Line Token
+
+  WiFi.begin(ssid, pass);
+    while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(400);
+  }
+  Serial.printf("\nWiFi connected\nIP : ");
+  Serial.println(WiFi.localIP());
+
+  Serial.print("Initializing SD card...");
+    pinMode(SS, OUTPUT);
+    if (!SD.begin(chipSelect)) {
+    Serial.println("initialization failed!");
+    return;
+    }
+    Serial.println("initialization done.");
+    myFile = SD.open("log.txt", FILE_WRITE); // เปิดไฟล์ที่ชื่อ log.txt เพื่อเขียนข้อมูล โหมด FILE_WRITE
+
 }
 
 void loop() {
@@ -44,6 +73,21 @@ void loop() {
     Blynk.virtualWrite(V10, 1);
     APIserver = "https://iot.korntk.com/iot-pro1/iot-api1.php?mq2=" + String(MQ) + "&flarm=" + String(Flam); // Construct API URL
     Serial.print(APIserver);
+
+    LINE.notifySticker(789, 10892);
+    LINE.notify("อันตราย! ตรวจพบไฟไหม้! กรุณาตรวจสอบโดยด่วน"); // ส่งข้อความ
+
+    if (myFile) {
+      Serial.print("Writing to log.txt...");
+      myFile.println("Alert!"); // สั่งให้เขียนข้อมูล
+      myFile.println("mq2=" + String(MQ) + " flarm=" + String(Flam));
+      myFile.close(); // ปิดไฟล์
+      Serial.println("done.");
+      } else {
+      // ถ้าเปิดไฟลืไม่สำเร็จ ให้แสดง error
+      Serial.println("error opening log.txt");
+      }
+
     WiFiClientSecure client; // Create a WiFiClientSecure object for HTTPS
     client.setInsecure(); // Allow insecure connections (not recommended in production)
     http.setFollowRedirects(HTTPC_STRICT_FOLLOW_REDIRECTS); // Follow strict redirects
